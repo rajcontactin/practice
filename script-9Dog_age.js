4c395d57590f05279e15a0517f6618adcbbcf9b9
raj donda <rajdonda1982007@gmail.com>
2026-01-20 11:43:42 +0530

    human age to dog age

---
+++ script-9Dog_age.js
@@ -0,0 +1,14 @@
+// writing my age or human age.
+let myAge =18;
+// created earlyYear.
+let earlyYears = 2;
+earlyYears *= 10.5;
+// console.log(earlyYears);
+//we already accounted the first two years.
+let  laterYears = myAge - 2;
+laterYears *= 4;
+console.log(earlyYears,laterYears);
+let myAgeinDogYears = earlyYears+laterYears;
+let myName = 'RAJ'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years, which is ${myAgeinDogYears} years old in dog years.
+`);
